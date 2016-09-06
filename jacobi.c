4c064669ae07506b26b83c6259e271845505e6f2
Hyungon_Ryu <Hyungon.Ryu@gmail.com>
2016-09-06 10:15:14 +0900

    Create jacobi.c
    laplace 2d jacobi example for profile and openacc

---
+++ jacobi.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#define NN 1024
+#define NM 1024
+
+double A[NN][NM];
+double Anew[NN][NM];
+
+int main(int argc, char** argv){
+    const int n = NN;
+    const int m = NM;
+    const int iter_max = 1000;
+    
+    const double tol = 1.0e-6;
+    double error     = 1.0;
+    
+    memset(A, 0, n * m * sizeof(double));
+    memset(Anew, 0, n * m * sizeof(double));
+        
+    for (int j = 0; j < n; j++)    {
+        A[j][0]    = 1.0;
+        Anew[j][0] = 1.0;
+    }
+    
+    printf("Jacobi relaxation Calculation: %d x %d mesh\n", n, m);
+    
+    int iter = 0;
+    
+    while ( error > tol && iter < iter_max )
+    {
+        error = 0.0;
+
+        for( int j = 1; j < n-1; j++)        {
+            for( int i = 1; i < m-1; i++ )            {
+                Anew[j][i] = 0.25 * ( A[j][i+1] + A[j][i-1]
+                                    + A[j-1][i] + A[j+1][i]);
+                error = fmax( error, fabs(Anew[j][i] - A[j][i]));
+            }
+        }
+        
+        for( int j = 1; j < n-1; j++)        {
+            for( int i = 1; i < m-1; i++ )            {
+                A[j][i] = Anew[j][i];    
+            }
+        }
+
+        if(iter % 100 == 0) printf("%5d, %0.6f\n", iter, error);
+        
+        iter++;
+    }
+
+return 0;
+}
